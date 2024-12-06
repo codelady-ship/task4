122d27bb839c5fd0a5bd80f9cd2acf3ffb242bc4
codelady-ship <jalilova.antiqa@gmail.com>
2024-12-06 17:15:59 +0400

    function

---
+++ task4.js
@@ -0,0 +1,43 @@
+/*Boş duranı Allah sevməz, kod yazın...
+Task-01
+İstifadəçi adını parametr kimi qəbul edən funksiya yazın.
+İstifadəçi adını parametr kimi qəbul edən və konsola
+«[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
+Funksiyanı 3 müxtəlif ad üçün çağırın.*/
+
+
+
+// -------------------------------------------------------------------------------------------------------------------
+
+
+/*Task-02
+Üç ədəddən ən böyüyünü təyin edən funksiya tərtib edin.
+Parametr olaraq üç ədəd qəbul edən və konsolda ən böyüyünü göstərən bir funksiya tərtib edin.
+Fərqli arqumentlərlə funksiyanı üç dəfə çağırın*/
+// -------------------------------------------------------------------------------------------------------------------
+
+/*Task-03
+Bütün hesabların cəmini rubl ilə mesaj çıxaran funksiya tərtib edin.
+Rubl və dollarla hesab qəbul edən və bütün hesablardakı məbləğin
+cəmin rublla göstərən funksiya yazın.
+Tutaq ki, müştərinin rubl hesabında 1200 rubl, dollar hesabında isə 20 dollar var.
+Bir dollar üçün məzənnə 75 rubl təşkil edir.
+Bu halda, funksiya belə mesaj göstərməlidir: Bütün depozitlər üzrə məbləğ: 2700 rubl.
+*/// -------------------------------------------------------------------------------------------------------------------
+
+/*Task-04
+Parametr kimi iki ədəd qəbul edən və onlardan ən kiçiyini qaytaran min() funksiyası tərtib edin.*/
+// -------------------------------------------------------------------------------------------------------------------
+
+/*Task-05
+Parametr kimi iki ədəd və riyazi əməliyyatın işarəsini qəbul edən və
+bu riyazi əməliyyatın nəticəsini qaytaran calc funksiyasını yazın.
+Məsələn, calc(4, 6, '+'); // 10 qaytaracaq*/
+// -------------------------------------------------------------------------------------------------------------------
+
+/*Task-06
+İstifadəçidən bir ay adı daxil etməsini tələb edən və həmin ayın neçə gün çəkdiyini göstərən bir proqram yazın. (switch-case ilə)*/
+// -------------------------------------------------------------------------------------------------------------------
+
+/*Task-07
+İstifadəçidən bir ədəd daxil etməsini tələb edən və bu ədədin Armstrong rəqəmi olub olmadığını yoxlayan bir proqram yazın. (dövrlə və if-else ilə)*/
